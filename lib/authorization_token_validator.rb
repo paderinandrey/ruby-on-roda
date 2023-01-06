1d2ea6b6c62249d865d1c4d33ea28cf71aabfdf6
paderinandrey <andy.paderin@gmail.com>
2023-01-07 02:58:53 +0300

    Authorization token generator

---
+++ lib/authorization_token_validator.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class AuthorizationTokenValidator
+  def initialize(authorization_token:, purpose:)
+    @authorization_token = authorization_token
+    @purpose = purpose
+  end
+
+  def call
+    return current_user if current_user && current_user.authentication_token == data[:authentication_token]
+
+    raise(ActiveSupport::MessageVerifier::InvalidSignature)
+  end
+
+  private
+
+  def data
+    @data ||= MessageVerifier.decode(message: @authorization_token, purpose: @purpose)
+  end
+
+  def current_user
+    @current_user ||= User.find(id: data[:user_id])
+  end
+end
