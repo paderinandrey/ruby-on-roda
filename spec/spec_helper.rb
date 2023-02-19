8477ce39bfaadb4b7a81f4c5baf1823e39ba6cdc
paderinandrey <andy.paderin@gmail.com>
2023-02-19 05:45:37 +0300

    Add Redis DatabaseCleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@
 
 ENV['RACK_ENV'] = 'test'
 
+require 'database_cleaner/redis'
+
 require_relative '../app'
 
 root_path = Pathname.new(File.expand_path('..', __dir__))
@@ -10,5 +12,16 @@ Dir[root_path.join('spec/support/**/*.rb')].each { |f| require f }
 RSpec.configure do |config|
   config.define_derived_metadata(file_path: /requests/) do |meta|
     meta[:type] = :request
+  end
+
+  config.before(:suite) do
+    DatabaseCleaner[:redis].db = ENV.fetch('REDIS_URL', nil)
+    DatabaseCleaner[:redis].clean_with(:deletion)
+  end
+
+  config.around do |example|
+    DatabaseCleaner[:redis].cleaning do
+      example.run
+    end
   end
 end
