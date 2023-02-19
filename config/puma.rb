aecc77db8e749d6a14c90f799c50556369581ede
paderinandrey <andy.paderin@gmail.com>
2023-02-19 05:47:08 +0300

    Add Puma config

---
+++ config/puma.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+workers Integer(ENV['WEB_CONCURRENCY'] || 0)
+threads_count = Integer(ENV['PUMA_MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup if defined?(DefaultRackup)
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
